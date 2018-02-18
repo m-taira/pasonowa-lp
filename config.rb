93c58941e8bdce5c3803744ce28ca9b29a657e6c
m.taira <taira.masaru@gmail.com>
2018-02-18 12:41:05 +0900

    first commit

---
+++ config.rb
@@ -0,0 +1,24 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "/Users/taira/pasonowa/LP/html/html"
+css_dir = "css"
+sass_dir = "src"
+images_dir = "images"
+generated_images_path = "/Users/taira/pasonowa/LP/html/generatedimages"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :expanded
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass src scss && rm -rf sass && mv scss sass
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp -r css wordpress:/var/www/sites/lp
+scp -r index.html wordpress:/var/www/sites/lp
